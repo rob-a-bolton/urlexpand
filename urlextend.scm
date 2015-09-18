798e0e6036dab9349f102e1c73594a416d42928c
Rob Bolton <badger.ea@gmail.com>
2015-09-18 01:18:01 +0100

    Added license.

--- urlextend.scm
+++ urlextend.scm
@@ -5,6 +5,18 @@
 (use utils)
 
 (define version "0.1")
+(define license "Copyright (c) 2015, Rob A. Bolton
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+  1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+  3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.")
 
 (define opts
   (list (args:make-option (i stdin)     #:none  "If set, read URLs from stdin")
@@ -21,7 +33,7 @@
     (exit 1))))
 
 (define print-version (lambda ()
-  (print-err-exit (conc "urlextend version " version))))
+  (print-err-exit (conc license #\newline #\newline "urlextend version " version))))
 
 (define print-help (lambda ()
   (print-err-exit (conc "Usage: " (car (argv)) " [options...] [urls]"
